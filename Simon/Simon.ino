befdf564723336410e74466f9dea3477b83b41ee
Rahshon <tuf70447@temple.edu>
2020-07-23 18:02:56 -0700

    Initial commit. Contains pseudo code and comments that establish the structure of the program. subject to change

---
+++ Simon/Simon.ino
@@ -0,0 +1,31 @@
+//These pins will trigger the lights (more lights = higher difficulty)
+
+//This variable (pause) will control how fast the lights will blink (decreases as lvls increase)
+
+//Random value will be used to to trigger lights at random
+
+//levels (min, max, current) will help dictate how long the light sequence gets and what lvl the player has reached
+
+//Array to hold the pin numbers (dictates the light sequence the user will repeat) 
+
+//Boolean: Use to check if the user has entered the sequence correctly
+
+
+void setup() {
+  // put your setup code here, to run once:
+  //set up pins and ready for OUTPUT
+
+}
+
+void loop() {
+  //put your main code here, to run repeatedly:
+
+  //initialize array with random numbers 
+
+  //while players current level < max level 
+  //iterate through the array (according to the current level) 
+    //trigger the corresponding lights
+
+    //wait
+
+}
